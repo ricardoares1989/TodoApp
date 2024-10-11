a3d16b5a04eaae8eac6003e267f84ae98119a794
Juan Gaines <juan.gaines@platzi.com>
2024-10-11 12:35:49 -0500

    global gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
