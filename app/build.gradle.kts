61ebd700056fc12dd5e1ad132f7862b1f5e1a54f
Juan Gaines <juan.gaines@platzi.com>
2024-10-15 17:55:46 -0500

    feat: master ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.dagger.hilt)
@@ -50,6 +49,11 @@ android {
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
