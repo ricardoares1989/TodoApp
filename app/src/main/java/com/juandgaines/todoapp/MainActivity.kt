1aa67c26205671f41ddd8fe37e6821eaea64e101
Juan Gaines <juan.gaines@platzi.com>
2024-10-14 19:46:58 -0500

    feat: categories

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.juandgaines.todoapp.ui.theme.TodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,9 +21,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    HelloWorld(modifier = Modifier.fillMaxSize()
+                        .padding(innerPadding))
                 }
             }
         }
     }
+}
+
+@Composable
+fun HelloWorld(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "Hello, World!",
+        modifier = modifier
+    )
 }
