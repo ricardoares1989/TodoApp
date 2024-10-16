f5f63e47a8ff2fdd84a74cfd4a79031d37832c3f
Juan Gaines <juan.gaines@platzi.com>
2024-10-15 19:06:25 -0500

    feat: Material Theme

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.compose.TodoAppTheme
+import com.juandgaines.todoapp.ui.theme.TodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
