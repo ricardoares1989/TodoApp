2cd766f50ae040dc5839ecd254e067f2b357c8b0
Juan Gaines <juan.gaines@platzi.com>
2024-10-16 15:20:00 -0500

    feat: domain models

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
@@ -66,6 +67,7 @@ class MainActivity : ComponentActivity() {
                     Text(
                         text = text,
                         modifier = Modifier
+                            .padding(innerPadding)
                             .fillMaxSize()
                     )
                 }
