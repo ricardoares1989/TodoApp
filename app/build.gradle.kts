58b342a73439d65bc36c035ac5dcdf0265239392
Juan Gaines <juan.gaines@platzi.com>
2024-10-17 15:08:19 -0500

    feat: compose viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-    //implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     //TODO: 3.5 Agregar libreria fuentes google
     implementation(libs.androidx.ui.text.google.fonts)
     debugImplementation(libs.androidx.ui.tooling)
