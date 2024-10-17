f1f55015ab11007c9052754d9b3fb2c6e03df805
Juan Gaines <juan.gaines@platzi.com>
2024-10-17 14:55:51 -0500

    feat: viewmodel libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     //TODO: 3.5 Agregar libreria fuentes google
     implementation(libs.androidx.ui.text.google.fonts)
     debugImplementation(libs.androidx.ui.tooling)
