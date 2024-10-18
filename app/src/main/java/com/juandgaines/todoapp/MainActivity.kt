992bba637705a374a18c7bfebaa3ed5cfbeb9cf1
Juan Gaines <juan.gaines@platzi.com>
2024-10-17 19:55:00 -0500

    feat: task screen

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoAppTheme() {
-                TaskScreen()
+
             }
         }
     }
