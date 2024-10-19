79db13cf4f680d84015925005ea1e6a900d9377e
Juan Gaines <juan.gaines@platzi.com>
2024-10-19 09:13:56 -0500

    feat: recomposition

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.juandgaines.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,11 +26,55 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TodoAppTheme() {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                var isShown by remember { mutableStateOf(false) }
 
+                Column (
+                    modifier = Modifier.padding(56.dp)
+                ) {
+                    if (isShown) {
+                        Text("This is a message")
+                    }
+                    Text(
+                        "Hello, world!",
+                        modifier = Modifier.clickable {
+                            isShown = !isShown
+                        }
+                    )
                 }
             }
         }
     }
 }
+
+@Composable
+fun HelloWorldView() {
+    var isShown by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(56.dp)
+    ) {
+        MessageText(isShown)
+        HelloWorldText {
+            isShown = !isShown
+        }
+    }
+}
+
+@Composable
+fun MessageText(isShown: Boolean) {
+    if (isShown) {
+        Text("This is a message")
+    }
+}
+
+@Composable
+fun HelloWorldText(onClick: () -> Unit) {
+    Text(
+        "Hello, world!",
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    )
+}
+
 
